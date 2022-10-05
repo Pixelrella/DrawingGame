b122d4f548bd55175d04f84548034efd0a7fc0d5
Christin Kalina <christin.kalina@gmail.com>
2022-10-05 23:30:36 +0200

    Unique pixel positions

--- Assets/Scripts/Gameplay/PixelGenerator.cs
+++ Assets/Scripts/Gameplay/PixelGenerator.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PixelGenerator : MonoBehaviour
@@ -5,6 +6,11 @@ public class PixelGenerator : MonoBehaviour
     [SerializeField] private Pixel _pixelPrefab;
     [SerializeField] private Collider2D _bounds;
     [SerializeField] private Grid _grid;
+
+    [SerializeField] private int numPixels = 100;
+    [SerializeField] private float cellSize = .5f;
+
+    private List<Vector3Int> occupiedCells = new List<Vector3Int>();
 
     private void Start()
     {
@@ -17,29 +23,42 @@ public class PixelGenerator : MonoBehaviour
         {
             Destroy(transform.GetChild(i).gameObject);
         }
+        occupiedCells.Clear();
 
-        for (int i = 0; i < 100; i++)
+
+        _grid.cellSize = new Vector3(cellSize, cellSize, 1);
+        for (int i = 0; i < numPixels; i++)
         {
-            SpawnPixelAtRandom();
+            SpawnPixelInCell(GetUniqueRandomCellPosition());
         }
     }
 
-    private void SpawnPixelAtRandom()
+    private Vector3Int GetUniqueRandomCellPosition()
     {
-        var randomX = Random.Range(_bounds.bounds.min.x, _bounds.bounds.max.x);
-        var randomY = Random.Range(_bounds.bounds.min.y, _bounds.bounds.max.y);
+        var randomX = Random.Range(_bounds.bounds.min.x + cellSize, _bounds.bounds.max.x + cellSize);
+        var randomY = Random.Range(_bounds.bounds.min.y + cellSize, _bounds.bounds.max.y + cellSize);
 
-        //TODO: do not spawn where other pixel is
+        var cellPosition = _grid.WorldToCell(new Vector3(randomX, randomY));
 
-        SpawnPixelAt(new Vector3(randomX, randomY));
+        var isOccupied = occupiedCells.Contains(cellPosition);
+
+        if (isOccupied)
+        {
+            return GetUniqueRandomCellPosition();
+        }
+
+        occupiedCells.Add(cellPosition);
+        return cellPosition;
     }
 
-    private void SpawnPixelAt(Vector3 position)
+    private void SpawnPixelInCell(Vector3Int cellPosition)
     {
-        Vector3Int cellPosition = _grid.WorldToCell(position);
-        position = _grid.CellToWorld(cellPosition);
+        var position = _grid.CellToWorld(cellPosition) - new Vector3(cellSize / 2, cellSize / 2, 0);
+
+        var scale = _grid.cellSize;
 
         var pixel = Instantiate(_pixelPrefab, position, Quaternion.identity, transform);
+        pixel.transform.localScale = scale;
         pixel.name = $"Pixel{cellPosition}";
     }
 
