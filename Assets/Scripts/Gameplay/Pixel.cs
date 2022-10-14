af6084e92f43758aa81468f2510188aacdca6f74
Christin Kalina <christin.kalina@gmail.com>
2022-10-14 21:45:59 +0200

    Pixel animation

--- Assets/Scripts/Gameplay/Pixel.cs
+++ Assets/Scripts/Gameplay/Pixel.cs
@@ -1,25 +1,31 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Animator))]
 public class Pixel : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer _sprite;
+
+    private Animator _animator;
 
     private Color _lastColor;
 
     private void Awake()
     {
         _lastColor = _sprite.color;
+        _animator = GetComponent<Animator>();
     }
 
     internal void ChangeColor(Color newColor)
     {
         _lastColor = _sprite.color;
         _sprite.color = newColor;
+        _animator.SetTrigger("Color");
     }
 
     internal void Undo()
     {
         _sprite.color = _lastColor;
+        _animator.SetTrigger("Undo");
     }
 }
